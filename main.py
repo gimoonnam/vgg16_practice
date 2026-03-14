7d542fbcafaf53e7b9b72e9872a656c06ea15e59
gmnam <34852540+gimoonnam@users.noreply.github.com>
2026-03-14 23:37:22 +0900

    .

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 """Train CIFAR10 with PyTorch."""
 
+import os
+import argparse
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -9,18 +12,17 @@ import torch.backends.cudnn as cudnn
 import torchvision
 import torchvision.transforms as transforms
 
-import os
-import argparse
-
 from models import *
 from utils import progress_bar
+
+# TODO:
+# 1. add wandb support
+# 2. edit progress bar to use tqdm
 
 
 parser = argparse.ArgumentParser(description="PyTorch CIFAR10 Training")
 parser.add_argument("--lr", default=0.1, type=float, help="learning rate")
-parser.add_argument(
-    "--resume", "-r", action="store_true", help="resume from checkpoint"
-)
+parser.add_argument("--resume", "-r", action="store_true", help="resume from checkpoint")
 args = parser.parse_args()
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -29,37 +31,37 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
 # Data
 print("==> Preparing data..")
+
+mean = (0.4914, 0.4822, 0.4465)
+std = (0.2023, 0.1994, 0.2010)
+
 transform_train = transforms.Compose(
     [
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize(mean, std),
     ]
 )
 
 transform_test = transforms.Compose(
     [
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize(mean, std),
     ]
 )
 
 trainset = torchvision.datasets.CIFAR10(
     root="./data", train=True, download=True, transform=transform_train
 )
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2
-)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(
     root="./data", train=False, download=True, transform=transform_test
 )
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2
-)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
-classes = (
+classes = [
     "plane",
     "car",
     "bird",
@@ -70,7 +72,7 @@ classes = (
     "horse",
     "ship",
     "truck",
-)
+]
 
 # Model
 print("==> Building model..")
@@ -89,6 +91,7 @@ if args.resume:
     net.load_state_dict(checkpoint["net"])
     best_acc = checkpoint["acc"]
     start_epoch = checkpoint["epoch"]
+
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
@@ -167,7 +170,43 @@ def test(epoch):
         best_acc = acc
 
 
+# from google.colab import userdata
+
+# # WANDB settings
+# os.environ["WANDB_API_KEY"] = userdata.get("wandb_key")
+# wandb.login()
+
+# # Start a new wandb run to track this script.
+# run = wandb.init(
+#     # Set the wandb entity where your project will be logged (generally your team name).
+#     entity="gimoonnam",
+#     # Set the wandb project where this run will be logged.
+#     project="vgg16",
+#     # Track hyperparameters and,
+#     name=run_name,
+#     config={
+#         "learning_rate": config.learning_rate,
+#         "architecture": "CNN",
+#         "dataset": "CIFAR-10",
+#         "epochs": config.num_epochs,
+#     },
+# )
+
+
 for epoch in range(start_epoch, start_epoch + 200):
     train(epoch)
     test(epoch)
     scheduler.step()
+
+    # # Log metrics to wandb (once per epoch)
+    # run.log(
+    #     {
+    #         "train_loss": avg_train_loss,
+    #         "val_loss": avg_val_loss,
+    #         "val_accuracy": avg_val_acc,
+    #         "lr": args.lr,
+    #     }
+    # )
+
+
+# run.finish()
